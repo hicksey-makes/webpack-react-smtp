369c2c5365233d8d66f32e190620e7f999cd9b79
hicksey <hicksey14@gmail.com>
2021-11-02 15:37:58 -0600

    webpack dev-server set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: path.resolve(__dirname, './src/index.js'),
+  devServer: {
+    static: './public',
+  },
   output: {
     path: path.resolve(__dirname, './public'),
     filename: 'bundle.js',
   },
-  devServer: {
-    contentBase: path.resolve(__dirname, './public'),
-  },
+
 };
