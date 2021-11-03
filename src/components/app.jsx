7d7eefd04ae7c3215d9446df177bbef2ea4ea105
hicksey <hicksey14@gmail.com>
2021-11-03 12:51:38 -0600

    add image capability using url-loader and asset/resource

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './app.scss';
+import Icon from '../Assets/Images/SMTPlogo.png';
 
 const App = () => {
+
   return (
     <div className="container">
       <div className="header">
         <h1>Welcome to my React application</h1>
       </div>
+      <div className="react-logo"><img src={Icon}></img></div>
     </div>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,17 @@ module.exports = {
       {
         test: /\.(s(a|c)ss)$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|png|svg|jpg)$/,
+        use: {
+          loader: 'url-loader'
+        },
+        type: 'javascript/auto'
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
       }
     ]
   },
