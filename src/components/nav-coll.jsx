651a2f7174a2fd817c25aa57a0e16f58259baa5b
hicksey <hicksey14@gmail.com>
2021-11-10 14:44:16 -0700

    place nav comp and define state variable to control hiding and showing

--- src/components/nav-coll.jsx
+++ src/components/nav-coll.jsx
@@ -1,9 +1,28 @@
 import React, {useState, useEffect} from 'react';
 
 function CollapseNav() {
+  const [navDisplay, setNavDisplay] = useState(false);
 
+  function toggleNavbar() {
+    setNavDisplay(!navDisplay);
+  }
 
-return <h1>Hello World</h1>;
+  return (
+    <div className={
+      "topnav " +
+      (navDisplay ? '' : 'remove-nav')} id="myTopnav">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Features</a></li>
+        <li><a href="#">Mobile App</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Twitter</a></li>
+        <li><a href="#">Facebook</a></li>
+        <li><a href="#"><img src="../Assets/Images/finding-brand.png" alt="Request a brand" /></a></li>
+        <li><a href="javascript:void(0);" className="icon" onClick="toggleNavbar()">&#9776;</a></li>
+      </ul>
+    </div>
+  );
 
 
 }
