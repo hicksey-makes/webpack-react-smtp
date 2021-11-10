7662dac1877d4ce7331c1bc588e2a829c0d23137
hicksey <hicksey14@gmail.com>
2021-11-10 13:48:14 -0700

    files and root node set up for header-nav react.js comp

--- src/components/nav-coll.jsx
+++ src/components/nav-coll.jsx
@@ -1,1 +1,11 @@
 import React, {useState, useEffect} from 'react';
+
+function CollapseNav() {
+
+
+return <h1>Hello World</h1>;
+
+
+}
+
+export default CollapseNav;
---
+++ src/header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CollapseNav from './components/nav-coll.jsx';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('nav')
+);
